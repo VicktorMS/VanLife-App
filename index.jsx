f05400f2cea11739d222ab07d19da8f48097a1c1
Victor Moraes <victor.moraesrj@gmail.com>
2023-07-03 11:55:04 -0300

    Using RouterProvider component instead of BrowserRouter to render all the routes

--- index.jsx
+++ index.jsx
@@ -6,7 +6,12 @@ import "css-maid";
 // import './reset.css'
 import "./index.css";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 import Home from "./src/pages/Home/Home";
 import About from "./src/pages/About/About";
@@ -18,45 +23,42 @@ import HostLayout from "./src/components/HostLayout/HostLayout";
 import Dashboard from "./src/pages/Host/Dashboard/Dashboard";
 import Income from "./src/pages/Host/Income/Income";
 import Reviews from "./src/pages/Host/Reviews/Reviews";
-import HostVans from './src/pages/Host/HostVans/HostVans'
+import HostVans from "./src/pages/Host/HostVans/HostVans";
 
 import HostVanDetail from "./src/pages/Host/HostVanDetail/HostVanDetail";
-import HostVanInfo from './src/pages/Host/HostVanInfo/HostVanInfo'
-import HostVanPhotos from './src/pages/Host/HostVanPhotos/HostVanPhotos'
-import HostVanPricing from './src/pages/Host/HostVanPricing/HostVanPricing'
+import HostVanInfo from "./src/pages/Host/HostVanInfo/HostVanInfo";
+import HostVanPhotos from "./src/pages/Host/HostVanPhotos/HostVanPhotos";
+import HostVanPricing from "./src/pages/Host/HostVanPricing/HostVanPricing";
 import NotFound from "./src/components/NotFound/NotFound.jsx";
-
 
 function App() {
   //BrowserRouter é um "Context Provider" ele provém contexto para todos os seus filhos
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="vans" element={<Vans />} />
-          <Route path="vans/:id" element={<VanDetail />} />
 
-          <Route path="host" element={<HostLayout/>}>
-            <Route index element={<Dashboard/>}/>
-            <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
-            <Route path="vans" element={<HostVans />} />
-            
-            <Route path="vans/:id" element={<HostVanDetail />}>
-              <Route index element={<HostVanInfo/>}/>
-              <Route path="photos" element={<HostVanPhotos/>}/>
-              <Route path="pricing" element={<HostVanPricing/>}/>
-            </Route>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="vans" element={<Vans />} />
+        <Route path="vans/:id" element={<VanDetail />} />
 
+        <Route path="host" element={<HostLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="income" element={<Income />} />
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="vans" element={<HostVans />} />
+
+          <Route path="vans/:id" element={<HostVanDetail />}>
+            <Route index element={<HostVanInfo />} />
+            <Route path="photos" element={<HostVanPhotos />} />
+            <Route path="pricing" element={<HostVanPricing />} />
           </Route>
-          <Route path="*" element={<NotFound/>}/>
         </Route>
-        
-      </Routes>
-    </BrowserRouter>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    )
   );
+  return <RouterProvider router={router} />;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
